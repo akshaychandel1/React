c8037a2240cefe7cb8bef3b4dcab2fb2459e6d1a
akshaychandel1 <chandel2102@gmail.com>
2025-07-07 17:18:10 +0530

    CONTROLLED COMPONNET

---
+++ 04_Controlle_Component/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  
+  return (
+    <>
+    {/* <h1>Controlled Component</h1>
+    <input type="text" placeholder='Enter Your Name' value={name} onChange={(event)=>{setName(event.target.value)}}/>
+    <br />
+    <input type="password" placeholder='Enter Your Password' value={password} onChange={(event)=>{setPassword(event.target.value)}} />
+    <br />
+    <input type="email" placeholder='Enter Your Email' value={email} onChange={(event)=>{setEmail(event.target.value)}} />
+    <br />
+
+    <button>Submit</button>
+    <button onClick={()=>{setEmail(''),setPassword(''),setName('')}}>Clear</button>
+
+    <h2>{name}</h2>
+    <h2>{password}</h2>
+    <h2>{email}</h2> */}
+    </> 
+  )
+}
+
+export default App
