098600142a2b458b12a90eb38296dbaed02929d5
akshaychandel1 <chandel2102@gmail.com>
2025-07-07 16:44:14 +0530

    text changed with change of input field by using states

--- 03_ReactBasics/vite-project/src/App.jsx
+++ 03_ReactBasics/vite-project/src/App.jsx
@@ -5,9 +5,21 @@ import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [val , setVal] = useState("")
 
   return (
     <>
+
+    <h1>Input Event Values</h1>
+    <input type="text" placeholder='Enter Input Value' value={val} onChange={event=>{setVal(event.target.value)}} />
+    <h1>{val}</h1>
+    <button onClick={()=>{setVal("")}}>Clear Every Value</button>
+
+
+
+
+
+{/*     
       <Wrapper color='blue'>
         <h1>Hello Wrapper Component</h1>
         <h2 style={{color:'red'}}>Please LogIn</h2>
@@ -15,7 +27,7 @@ function App() {
       <Wrapper color='orange'>
         <h1>Hello I am Orange</h1>
         <h2 style={{color:'red'}}>Please LogIn</h2>
-      </Wrapper>
+      </Wrapper> */}
       {/* <User name='Akshay Chandel' name1='Shikhu '/> */}
     </>
   )
